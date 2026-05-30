98f3a3dc6f6a66e83fd5b2f4bb7fe1a7ba1686c8
parvezmahmudlalin <parvezmahmudlalin@gmail.com>
2026-05-30 12:22:33 +0600

    first commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,10 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Outfit } from "next/font/google";
 import "./globals.css";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const OutfitFont = Outfit({
   subsets: ["latin"],
 });
 
@@ -20,9 +17,14 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${OutfitFont.className} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body >
+        <Navbar></Navbar>
+        {children}
+        <Footer></Footer>
+        </body>
+
     </html>
   );
 }
